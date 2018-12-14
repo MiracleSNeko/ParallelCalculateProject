645daa712ee3720c31f59177bc219673f9067b9f
Yuchien Lin <39214153+MiracleSNeko@users.noreply.github.com>
2018-12-15 01:15:19 +0900

    generate_random_matrix return a vector instead of a matrix

--- generate_random_matrix.f90
+++ generate_random_matrix.f90
@@ -3,7 +3,7 @@ program generate_random_matrix
     implicit none
 
     ! character :: s
-    integer :: i
+    integer :: i, j, k
     integer, parameter :: n = 1024
     real(4), dimension(n, n) :: random_matrix
     call random_seed()
@@ -11,9 +11,13 @@ program generate_random_matrix
     random_matrix = 100 * random_matrix
 
     open(20, file = 'matrix', access = 'direct', &
-    &  form = 'unformatted', recl = 4 * n)
+    &  form = 'unformatted', recl = 4)
+    k = 0
     do i = 1, n
-        write(20, rec = i) random_matrix(i, :)
+        do j = 1, n
+            k = k + 1
+            write(20, rec = k) random_matrix(i, j)
+        end do
     end do
     close(20)
 
